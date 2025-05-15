d0bffceb776d63dc544f366a10c0a4fb02d6c668
Md-Mosabbir <kmosabbir@gmail.com>
2025-05-15 20:44:45 +0600

    feat: add Resend notification provider service for order confirmation emails
    - Implemented ResendNotificationProviderService to handle email notifications using Resend API.
    - Created order-placed email template and integrated it into the notification service.
    - Added orderPlacedHandler subscriber to trigger email notifications upon order placement.
    - Developed send-order-confirmation workflow to fetch order details and send notifications.
    - Introduced sendNotificationStep to facilitate notification sending within workflows.
    - Added necessary dependencies for email rendering and notification handling.

--- my-medusa-store/medusa-config.ts
+++ my-medusa-store/medusa-config.ts
@@ -61,7 +61,23 @@ module.exports = defineConfig({
     },
     {
       resolve: "./src/modules/ingredient",
-    }
+    },
+        {
+      resolve: "@medusajs/medusa/notification",
+      options: {
+        providers: [
+          {
+            resolve: "./src/modules/resend",
+            id: "resend",
+            options: {
+              channels: ["email"],
+              api_key: process.env.RESEND_API_KEY,
+              from: process.env.RESEND_FROM_EMAIL,
+            },
+          },
+        ],
+      },
+    },  
 
   ],
 });
