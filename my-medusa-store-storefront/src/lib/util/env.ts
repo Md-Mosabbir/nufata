f863504850c178b5bca060bae5eab62733120f5f
Md-Mosabbir <kmosabbir@gmail.com>
2025-08-06 01:25:59 +0600

    Hell

--- my-medusa-store-storefront/src/lib/util/env.ts
+++
@@ -1,5 +0,0 @@
-export const getBaseURL = () => {
-  return process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
-}
-
-export const NEXT_PUBLIC_SUPABASE_PUBLIC_URL = process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_URL;
