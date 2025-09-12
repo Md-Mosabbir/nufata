444cb36afb99d5137c84fd374c2092eb2dbac919
Md-Mosabbir <kmosabbir@gmail.com>
2025-09-12 06:23:20 +0600

    feat: enhance medusa configuration with admin settings and improved environment variable handling

--- medusa-config.ts
+++ medusa-config.ts
@@ -3,15 +3,20 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils';
 loadEnv(process.env.NODE_ENV || 'development', process.cwd());
 
 module.exports = defineConfig({
+    admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    backendUrl: process.env.MEDUSA_BACKEND_URL
+  },
   projectConfig: {
     redisUrl: process.env.REDIS_URL,
     databaseUrl: process.env.DATABASE_URL,
+    workerMode: process.env.MEDUSA_WORKER_MODE  as "shared" | "server"| "worker" || "shared",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+      jwtSecret: process.env.JWT_SECRET ,
+      cookieSecret: process.env.COOKIE_SECRET ,
     }
   },
 
